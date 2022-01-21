819c70cb0e07acf7702414443d88c03c41bdff69
AMIRUL MUKMININ <39360383+amirul404@users.noreply.github.com>
2022-01-21 11:29:21 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let path = require('path')
 let fetch = require('node-fetch')
 let levelling = require('../lib/levelling')
-const thumb = fs.readFileSync('./src/thumb.jpeg')
+const thumb = fs.readFileSync('./src/thumb.jpg')
 let tags = {
   'main': '🄼🄰🄸🄽',
   'game': '🄶🄰🄼🄴',
